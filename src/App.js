908da34300cf393e0efca4b57cb3f92099077f7a
masa-maker <76035698+masa-maker@users.noreply.github.com>
2024-09-05 15:19:30 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+    const onClickUsers = () => {
+      axios.get("https://jsonplaceholder.typicode.com/users").then((rez) => {
+        console.log(rez.data);
+      });
+    };
+    
+  const onClickUser1 = () => {
+    alert("user1");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
     </div>
   );
 }
